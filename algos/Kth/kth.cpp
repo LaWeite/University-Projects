938c167afb9252279ef6830acf18542fef4f7d9e
Кручинин Евгений <96009716+LaWeite@users.noreply.github.com>
2024-03-23 17:27:01 +0300

    Update kth.cpp

--- algos/Kth/kth.cpp
+++ algos/Kth/kth.cpp
@@ -1,48 +1,51 @@
 #include <fstream>
+#include <chrono>
 #include <iostream>
 
-// Функция для обмена двух элементов
-static void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
+static void quickSelect(int* array, int left, int right, int k1, int k2) {
+    if (left >= right)
+    {
+        return;
+    }
 
-// Будем использовать разбиение Hoare
-static int hoarePartition(int* arr, int left, int right) {
-    int pivot = arr[left];
-    int i = left - 1;
-    int j = right + 1;
-    while (true) {
-        do {
-            i++;
-        } while (arr[i] < pivot);
-        do {
-            j--;
-        } while (arr[j] > pivot);
-        if (i >= j) {
-            return j;
+    int l = left;
+    int r = right;
+    int pivot = array[l + (r - l) / 2];
+
+    while (l <= r)
+    {
+        while (array[l] < pivot)
+        {
+            ++l;
         }
-        swap(arr[i], arr[j]);
+        while (array[r] > pivot)
+        {
+            --r;
+        }
+        if (l >= r)
+        {
+            break;
+        }
+        if (l < r)
+        {
+            std::swap(array[l], array[r]);
+            ++l;
+            --r;
+        }
     }
-}
 
-static void quickSelect(int* arr, int left, int right, int k1, int k2) {
-    if (left >= right) return;
-
-    int pivotIndex = hoarePartition(arr, left, right);
-
-    if (k1 <= pivotIndex) {
-        quickSelect(arr, left, pivotIndex, k1, k2);
+    if (k1 <= r) {
+        quickSelect(array, left, r, k1, k2);
     }
-    if (k2 >= pivotIndex + 1) {
-        quickSelect(arr, pivotIndex + 1, right, k1, k2);
+    if (k2 >= r) {
+        quickSelect(array, r + 1, right, k1, k2);
     }
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3)
-    {
+    auto begin = std::chrono::steady_clock::now();
+
+    if (argc != 3) {
         return 1;
     }
 
@@ -68,6 +71,8 @@ int main(int argc, char* argv[]) {
     int x2;
     fileInput >> n >> k1 >> k2 >> A >> B >> C >> x1 >> x2;
 
+    fileInput.close();
+
     int* array = new int[n];
     array[0] = x1;
     array[1] = x2;
@@ -76,10 +81,8 @@ int main(int argc, char* argv[]) {
         array[i] = A * array[i - 2] + B * array[i - 1] + C;
     }
 
-    // Находим k1-ю до k2-ю порядковые статистики
     quickSelect(array, 0, n - 1, k1 - 1, k2 - 1);
 
-    // Выводим результаты
     for (int i = k1 - 1; i < k2; ++i) {
         if (i == k2 - 1)
         {
@@ -89,6 +92,13 @@ int main(int argc, char* argv[]) {
         outputStream << array[i] << " ";
     }
 
+    outputStream.close();
     delete[] array;
+
+    auto end = std::chrono::steady_clock::now();
+
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    std::cout << "The time: " << elapsed_ms.count() << " ms\n";
+
     return 0;
 }
